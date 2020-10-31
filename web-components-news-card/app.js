b2df44df2ef7499752097bb6f90177527ae9e3c4
Michael Movsesov <michael.movsesov@gmail.com>
2020-10-31 00:55:45 -0700

    Udated with reddit fetch

--- web-components-news-card/app.js
+++ web-components-news-card/app.js
@@ -1,1 +1,25 @@
-import './news-card.js';
+import NewsCard from './news-card.js';
+
+async function fetchNews() {
+  const response = await fetch('https://www.reddit.com/r/upliftingnews/.json');
+  return response.json();
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const headlines = await fetchNews();
+  const newsContainer = document.querySelector('#news');
+
+  headlines.data.children.forEach((headline) => {
+    // Create a new element instance
+    const newsCard = new NewsCard();
+
+    // Set attributes
+    newsCard.setAttribute('title', headline.data.title);
+    newsCard.setAttribute('href', headline.data.url);
+    newsCard.setAttribute('imageSrc', headline.data.thumbnail);
+    newsCard.innerText = headline.data.title;
+
+    // Append to DOM
+    newsContainer.appendChild(newsCard);
+  });
+});
--- web-components-news-card/news-card.js
+++ web-components-news-card/news-card.js
@@ -1,76 +1,87 @@
-const template = document.createElement('template');
-
-template.innerHTML = `
-  <style>
-    .card {
-      display: block;
-      position: relative;
-      font: inherit;
-      background-color: #ffffff;
-      border-radius: 5px;
-      transition: box-shadow 0.2s ease-in-out, transform 0.2s ease-in-out;
-      cursor: pointer;
-      overflow: hidden;
-    }
-
-    .card:hover {
-      transform: translateY(-10px);
-      box-shadow: 0 30px 50px -17px rgba(0,0,0,.3), 0 3px 20px rgba(0,0,0,.1);
-    }
-
-    .card-inner {
-      min-height: 180px;
-      max-height: 180px;
-      min-width: 400px;
-      display: flex;
-      align-items: stretch;
-      height: 100%;
-      width: 100%;
-    }
-    
-
-    .img {
-      width: 40%;
-      background-size: cover;
-      background-repeat: no-repeat;
-    }
-    .content {
-      padding: 1rem;
-      width: 60%;
-    }
-
-    .title {
-      font-size: 1.2rem;
-      margin-top: 0;
-      font-weight: 900;
-    }
-  </style>
-
-  <article class="card">
-    <div class="card-inner">
-      <div class="img"></div>
-      <div class="content">
-        <h1 class="title"></h1>
-        <p><slot /></p>
-      </div>
-    </div>
-  </article>
-`;
-class NewsCard extends HTMLElement {
+export default class NewsCard extends HTMLElement {
   constructor() {
     super();
 
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
     this.render();
   }
 
   render() {
-    this.shadowRoot.querySelector('h1').innerText = this.getAttribute('title');
+    const template = document.createElement('template');
 
-    this.shadowRoot.querySelector(
-      '.img'
-    ).style.backgroundImage = `url('${this.getAttribute('imageSrc')}')`;
+    template.innerHTML = `
+      <style>
+        .card {
+          display: block;
+          position: relative;
+          font: inherit;
+          background-color: #ffffff;
+          border-radius: 5px;
+          transition: box-shadow 0.2s ease-in-out, transform 0.2s ease-in-out;
+          cursor: pointer;
+          overflow: hidden;
+          width: 100%;
+        }
+
+        .card:hover {
+          transform: translateY(-10px);
+          box-shadow: 0 30px 50px -17px rgba(0,0,0,.3), 0 3px 20px rgba(0,0,0,.1);
+        }
+
+        .card-inner {
+          display: flex;
+          align-items: stretch;
+          min-height: 180px;
+          max-height: 180px;
+          min-width: 400px;
+          height: 100%;
+          width: 100%;
+        }
+        
+        .img {
+          width: 40%;
+          object-fit: cover;
+        }
+
+        .content {
+          padding: 1rem;
+          width: 60%;
+        }
+
+        .content p {
+          display: -webkit-box;
+          -webkit-line-clamp: 3; /* number of lines to show */
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+
+        .title {
+          font-size: 1.2rem;
+          margin-top: 0;
+          font-weight: 900;
+          display: -webkit-box;
+          -webkit-line-clamp: 2; /* number of lines to show */
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      </style>
+
+      <article class="card">
+        <div class="card-inner">
+          <img src="${this.getAttribute('imageSrc')}" class="img" />
+          <div class="content">
+            <h1 class="title">${this.getAttribute('title')}</h1>
+            <p><slot /></p>
+          </div>
+        </div>
+      </article>
+    `;
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     this.shadowRoot
       .querySelector('article')
