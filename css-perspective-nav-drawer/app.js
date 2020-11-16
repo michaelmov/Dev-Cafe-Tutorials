cb4d9ec8011e3329e4e8d7df8ce67f7103f4eef4
Michael Movsesov <michael.movsesov@gmail.com>
2020-11-15 23:16:01 -0800

    Made main section rotate with perspective. Yay!

--- css-perspective-nav-drawer/app.js
+++ css-perspective-nav-drawer/app.js
@@ -1,12 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const body = document.querySelector('body');
-  const menuBtn = document.querySelector('#menu-btn');
+// Cache UI elements
+const body = document.querySelector('body');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
 
+// Handlers
+function openMenu() {
+  if (!body.classList.contains('is-open')) body.classList.add('is-open');
+}
+
+function closeMenu() {
+  if (body.classList.contains('is-open')) body.classList.remove('is-open');
+}
+
+// Event listeners
+document.addEventListener('DOMContentLoaded', () => {
   menuBtn.addEventListener('click', (e) => {
-    if (body.classList.contains('is-open')) {
-      body.classList.remove('is-open');
-    } else {
-      body.classList.add('is-open');
-    }
+    openMenu();
+  });
+
+  closeBtn.addEventListener('click', (e) => {
+    closeMenu();
   });
 });
