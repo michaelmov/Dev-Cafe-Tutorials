e7c24fdc9d60cbf9566b3ba597c1bcbbeb1c5cd0
Michael Movsesov <michael.movsesov@gmail.com>
2021-01-03 15:37:47 -0800

    Revert refactor

--- vue3-global-state/src/components/Tasks.vue
+++ vue3-global-state/src/components/Tasks.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-96 max-h-96 p-8 bg-white overflow-auto rounded-md">
     <add-task-input class="mb-8"/>
-    <p v-if="allTasks?.length < 1" class="text-center text-gray-400">No tasks available</p>
+    <p v-if="!allTasks || allTasks.length < 1" class="text-center text-gray-400">No tasks available</p>
     <task-item v-else v-for="task in allTasks" :key="task.id" :task="task" />
   </div>
 </template>
